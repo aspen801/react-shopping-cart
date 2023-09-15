6bc07647aaf476984254989c54903821bee92e24
aspen801 <boyko.bogdan45@gmail.com>
2023-09-15 23:22:27 +0300

    added slider to homepage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeroSlider from "../../components/HeroSlider/HeroSlider";
+import "./HomePage.css"
+
+
+const HomePage = () => {
+    
+    return (
+        <div className="main-wrapper">
+            <HeroSlider />
+        </div>
+    )
+}
+
+export default HomePage
