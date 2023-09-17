146a6524e68e97137f1c798c56384936b42b33dc
aspen801 <boyko.bogdan45@gmail.com>
2023-09-17 12:39:30 +0300

    added product cards

---
+++ src/components/Products/ProductCard/ProductCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./ProductCard.css";
+
+const ProductCard = ({product, productVariants}) => {
+    
+    if(!product){
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+    
+    const productImage = product.featuredImage.url;
+    const variantPrice = productVariants.edges[0].node.price.amount;
+
+    return (
+        <div className="card__wrapper">
+            <div className="card__photo-area">
+                <img src={productImage} alt="" />
+            </div>
+            <div className="card__info-area"> 
+                <div className="card__info-area-title">
+                    <h3>{product.title}</h3>
+                </div>  
+                <div className="card__info-area-description">
+                    <p>{product.description ? product.description : 'No desctiption yet'}</p>
+                </div>
+                <div className="card__info-area-price">
+                    <p>{"$ " + variantPrice}</p>
+                    <button>Add to cart</button>
+                </div>    
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard;
