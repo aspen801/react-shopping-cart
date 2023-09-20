be9be0be6ddd93e8e1b41f8286fa96a77a8f5338
aspen801 <boyko.bogdan45@gmail.com>
2023-09-20 22:32:21 +0300

    added cart context

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,88 @@
+import React, { useState, createContext } from "react";
+
+export const CartContext = createContext({
+  cartItems: [],
+  addToCart: () => {},
+  removeFromCart: () => {},
+  fullyRemoveFromCart: () => {},
+  getCartTotal: () => {},
+});
+
+const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    const isItemInCart = cartItems.find(
+      (cartItem) => cartItem.product.id === product.id,
+    );
+
+    if (isItemInCart) {
+      setCartItems(
+        cartItems.map((cartItem) =>
+          cartItem.product.id === product.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem,
+        ),
+      );
+    } else {
+      setCartItems([...cartItems, { product, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (product) => {
+    const isItemInCart = cartItems.find(
+      (cartItem) => cartItem.product.id === product.id,
+    );
+
+    if (isItemInCart) {
+      if (isItemInCart.quantity === 1) {
+        setCartItems(
+          cartItems.filter((cartItem) => cartItem.product.id !== product.id),
+        );
+      } else {
+        setCartItems(
+          cartItems.map((cartItem) =>
+            cartItem.product.id === product.id
+              ? { ...cartItem, quantity: cartItem.quantity - 1 }
+              : cartItem,
+          ),
+        );
+      }
+    }
+  };
+
+  const fullyRemoveFromCart = (product) => {
+    const isItemInCart = cartItems.find(
+      (cartItem) => cartItem.product.id === product.id,
+    );
+
+    if (isItemInCart) {
+      setCartItems(
+        cartItems.filter((cartItem) => cartItem.product.id !== product.id),
+      );
+    }
+  };
+
+  const getCartTotal = () => {
+    return cartItems.reduce(
+      (total, cartItem) => total + cartItem.product.price * cartItem.quantity,
+      0,
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        fullyRemoveFromCart,
+        getCartTotal,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
