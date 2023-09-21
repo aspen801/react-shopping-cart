4162258ce9a1a396fae954233da8044203c77fa2
aspen801 <boyko.bogdan45@gmail.com>
2023-09-21 21:05:58 +0300

    added all components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header'
-import HomePage from './pages/HomePage/HomePage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage/HomePage";
+import CartPage from "./pages/CartPage/CartPage";
+import CartProvider from "./context/CartContext";
 
 function App() {
-
+  
   return (
     <>
-      <Header />
-      <HomePage />
+      <CartProvider>
+        <Header />
+        <ToastContainer />
+        <Routes>
+          <Route path={"/"} element={<HomePage />}/>
+          <Route path={"/cart"} element={<CartPage />}/>
+        </Routes>
+        <Footer />
+      </CartProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
