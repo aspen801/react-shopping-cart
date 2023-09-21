a807929e9a10131c2b8a14c21254377f49cf5eb8
aspen801 <boyko.bogdan45@gmail.com>
2023-09-21 21:07:13 +0300

    reworked header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,48 @@
-import React from "react";
-import { Link } from 'react-router-dom';
-import "./Header.css"
+import React, { useContext } from "react";
+import { CartContext } from "../../context/CartContext.jsx";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
-import logoImage from "/assets/paper-plane.svg"
-import searchIcon from "/assets/search.svg"
-import cartIcon from "/assets/shopping-cart.svg"
-
+import logoImage from "/assets/paper-plane.svg";
+import searchIcon from "/assets/search.svg";
+import cartIcon from "/assets/shopping-cart.svg";
 
 const Header = () => {
+  const { getCartQuantity } = useContext(CartContext);
 
-    return (
-        <header>
-            <div className="header__logo-container">
-                <Link to={"/"}>
-                    <img src={logoImage} alt="" />
-                    <h1>Mock<span style={{color: '#537977'}}>shop</span>.</h1>
-                </Link>
-                
+  return (
+    <header>
+      <div className="header__logo-container">
+        <Link to={"/"}>
+          <img src={logoImage} alt="" />
+          <h1>
+            Mock<span style={{ color: "#537977" }}>shop</span>.
+          </h1>
+        </Link>
+      </div>
+      <div className="header__navigation">
+        <a href="#">Men</a>
+        <a href="#">Woman</a>
+        <a href="#">Sport</a>
+        <a href="#">Blank</a>
+        <a href="#">Blank</a>
+        <a href="#">Blank</a>
+      </div>
+      <div className="header__side-menu">
+        <button className="header__side-menu-search-button-container">
+          <img src={searchIcon} alt="" />
+        </button>
+        <Link to={"/cart"}>
+          <button className="header__side-menu-cart-button-container">
+            <img src={cartIcon} alt="" />
+            <div className="header__side-menu-cart-counter">
+              <p>{getCartQuantity()}</p>
             </div>
-            <div className="header__navigation">
-                <a href="#">Men</a>
-                <a href="#">Woman</a>
-                <a href="#">Sport</a>
-                <a href="#">Blank</a>
-                <a href="#">Blank</a>
-                <a href="#">Blank</a>
-            </div>
-            <div className="header__side-menu">
-                <button><img src={searchIcon} alt="" /></button>
-                <Link to={"/cart"}><button><img src={cartIcon} alt="" /></button></Link>
-            </div>
-        </header>
-    )
-}
+          </button>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
