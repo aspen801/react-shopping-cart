e0a00b69d6f94d6a89c10f01b6e92182295bfd00
aspen801 <boyko.bogdan45@gmail.com>
2023-09-21 21:02:59 +0300

    added transformData function

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,11 +6,25 @@ const Products = () => {
 
     const [products, setProducts] = useState([]);
 
+    const transformData = (responseData) => {
+        const transformedData = responseData.map((item) => {
+            return {
+                id: item.node.id,
+                title: item.node.title,
+                description: item.node.description,
+                image: item.node.featuredImage.url,
+                price: item.node.variants.edges[0].node.price.amount
+            }
+        })
+        
+        return transformedData;
+    }
+
     useEffect(() => {
         fetch('https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}')
             .then((response) => response.json())
             .then((responseData) => {
-                setProducts(responseData.data.products.edges);
+                setProducts(transformData(responseData.data.products.edges));
             })
             .catch((err) => console.error(err));
     }, [])
@@ -23,7 +37,7 @@ const Products = () => {
             </div>
             <div className="products__cards-area">
                 {products.map((product, index) => 
-                    <ProductCard key={index} product={product.node} productVariants={product.node.variants}/>
+                    <ProductCard key={index} product={product} /*productVariants={product.node.variants}*//>
                 )}
             </div>
         </div>
