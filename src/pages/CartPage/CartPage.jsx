be75abe6d53b41510b47408f71c8f489ab6ba89e
aspen801 <boyko.bogdan45@gmail.com>
2023-10-03 22:03:58 +0300

    cart page refactor

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -5,45 +5,68 @@ import "./CartPage.css";
 import PlusButton from "/assets/plus.svg";
 import MinusButton from "/assets/minus.svg";
 
+const EmptyCartMessage = () => (
+    <div className="cart__empty-message">
+        <h1>Cart is empty right now 😔</h1>
+    </div>
+);
+
+const CartItem = ({ cartItem, index, removeFromCart, addToCart, fullyRemoveFromCart }) => (
+  <div key={index}>
+    <div className="cart__item">
+      <div className="cart__img-container">
+        <img src={cartItem.product.image} alt="" />
+        <div className="cart__item-img-price-container">
+            <p>
+              $ {cartItem.product.price}
+            </p>
+        </div>
+      </div>
+      <div className="cart__item-info">
+        <div className="cart__item-info-top">
+          <h1>{cartItem.product.title}</h1>
+          <div className="cart__quantity-counter">
+            <button onClick={() => {removeFromCart(cartItem.product)}}><img src={MinusButton} alt="" /></button>
+            <div><h1>{cartItem.quantity}</h1></div>
+            <button onClick={() => {addToCart(cartItem.product)}}><img src={PlusButton} alt="" /></button>
+          </div>
+          <div className="cart__item-info-top-price-container">
+            <p>
+              $ {cartItem.product.price}
+            </p>
+          </div>
+        </div>
+        <div className="cart__item-info-bottom">
+          <h1>{cartItem.product.description}</h1>
+          <button onClick={() => {fullyRemoveFromCart(cartItem.product)}}>Remove</button>
+        </div>
+      </div>
+    </div>
+    <hr className="cart__divider" />
+  </div>
+);
+
+const SummaryArea = ({ getCartTotal, shippingCost }) => (
+  <div className="cart__summary-area">
+    <h1>Summary</h1>
+    <p><span>Subtotal:</span> <span className="price">{getCartTotal()}.00 $</span></p>
+    <hr className="cart__summary-divider"/>
+    <p><span>Shipping:</span> <span className="price">{shippingCost}.00 $</span></p>
+    <hr className="cart__summary-divider"/>
+    <p><span>Order total:</span> <span className="price">{getCartTotal() + shippingCost}.00 $</span></p>
+    <div className="cart__summary-checkout-button">
+      <button>Checkout</button>
+    </div>
+  </div>
+);
+
 const CartPage = () => {
   const { cartItems, addToCart, removeFromCart, fullyRemoveFromCart, getCartTotal } = useContext(CartContext);
 
   //imagine it fetch from server :)
   const shippingCost = 15;
 
-  if(cartItems.length === 0){
-    return (
-      <div className="cart__wrapper">
-      <div className="cart__content">
-        <div className="cart__text-area">
-          <h1>Shopping cart</h1>
-        </div>
-        <hr className="cart__divider" />
-        <div className="cart__checkout-area">
-          <div className="cart__item-area">
-            <div className="cart__empty-message">
-              <h1>Cart is empty right now 😔</h1>
-            </div>
-          </div>
-          <hr className="cart__summary-area-divider" />
-          <div className="cart__summary-area">
-            <h1>Summary</h1>
-            <p><span>Subtotal:</span> <span className="price">{getCartTotal()}.00 $</span></p>
-            <hr className="cart__summary-divider"/>
-            <p><span>Shipping:</span> <span className="price">{shippingCost}.00 $</span></p>
-            <hr className="cart__summary-divider"/>
-            <p><span>Order total:</span> <span className="price">{getCartTotal() + shippingCost}.00 $</span></p>
-            <div className="cart__summary-checkout-button">
-              <button>Checkout</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    )
-  }
-
-  return  (
+  return (
     <div className="cart__wrapper">
       <div className="cart__content">
         <div className="cart__text-area">
@@ -52,53 +75,12 @@ const CartPage = () => {
         <hr className="cart__divider" />
         <div className="cart__checkout-area">
           <div className="cart__item-area">
-            {cartItems.map((cartItem, index) => (
-              <div key={index}>
-                <div className="cart__item">
-                  <div className="cart__img-container">
-                    <img src={cartItem.product.image} alt="" />
-                    <div className="cart__item-img-price-container">
-                        <p>
-                          $ {cartItem.product.price}
-                        </p>
-                    </div>
-                  </div>
-                  <div className="cart__item-info">
-                    <div className="cart__item-info-top">
-                      <h1>{cartItem.product.title}</h1>
-                      <div className="cart__quantity-counter">
-                        <button onClick={() => {removeFromCart(cartItem.product)}}><img src={MinusButton} alt="" /></button>
-                        <div><h1>{cartItem.quantity}</h1></div>
-                        <button onClick={() => {addToCart(cartItem.product)}}><img src={PlusButton} alt="" /></button>
-                      </div>
-                      <div className="cart__item-info-top-price-container">
-                        <p>
-                          $ {cartItem.product.price}
-                        </p>
-                      </div>
-                    </div>
-                    <div className="cart__item-info-bottom">
-                      <h1>{cartItem.product.description}</h1>
-                      <button onClick={() => {fullyRemoveFromCart(cartItem.product)}}>Remove</button>
-                    </div>
-                  </div>
-                </div>
-                <hr className="cart__divider" />
-              </div>
+            {cartItems.length === 0 ? <EmptyCartMessage/> : cartItems.map((cartItem, index) => (
+              <CartItem  key={index} addToCart={addToCart} removeFromCart={removeFromCart} fullyRemoveFromCart={fullyRemoveFromCart} cartItem={cartItem} />
             ))}
           </div>
           <hr className="cart__summary-area-divider" />
-          <div className="cart__summary-area">
-            <h1>Summary</h1>
-            <p><span>Subtotal:</span> <span className="price">{getCartTotal()}.00 $</span></p>
-            <hr className="cart__summary-divider"/>
-            <p><span>Shipping:</span> <span className="price">{shippingCost}.00 $</span></p>
-            <hr className="cart__summary-divider"/>
-            <p><span>Order total:</span> <span className="price">{getCartTotal() + shippingCost}.00 $</span></p>
-            <div className="cart__summary-checkout-button">
-              <button>Checkout</button>
-            </div>
-          </div>
+          <SummaryArea getCartTotal={getCartTotal} shippingCost={shippingCost}/>
         </div>
       </div>
     </div>
